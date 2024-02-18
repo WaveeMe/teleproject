1a8f4b41fe959bcce7d345d6191e39675b26cfa9
WaveeMe <tahkbkapmahe@gmail.com>
2024-02-18 21:09:53 +0000

    Final Variant With Colorama

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from pyrogram import Client
 from pyrogram.errors import FloodWait
-import time
-import random
-
+import time, random, os, colorama
 from config import *
-
+import logging
 
 app = Client("account", api_id=api_id, api_hash=api_hash, system_version="4.16.30-vxmy_text")
+logging.disable()
 
 
 def send_messages():
@@ -15,14 +14,15 @@ def send_messages():
             for group in groups:
                 try:
                     app.send_message(group, text=text)
-                    print(f"Sended to {group}")
-                except FloodWait:
+                    print(colorama.Fore.GREEN, f"Sended to {group}")
+                except Exception:
                     time.sleep(10)
                     app.send_message(group, text=text)
-                    print(f"Sended to {group} with FloodWait Error")
+                    print(colorama.Fore.YELLOW, f"Sended to {group} with Error")                    
                 time.sleep(random.randint(min_sleep, max_sleep))
 
 def check_info():
+    os.system("cls")
     print(f"\nГруппы в которые будет рассылка: {groups}\n"
           f"Текст: {text}\n"
           f"Минимальная задержка: {min_sleep}, максимальная {max_sleep}\n")
